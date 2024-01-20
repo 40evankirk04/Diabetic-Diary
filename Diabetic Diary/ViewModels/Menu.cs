4c2bf3836db5dfbb7eaa09b89a6d524b9de499f0
Ivan <evankirk@vk.com>
2024-01-21 03:24:59 +0500

    Добавьте файлы проекта.

---
+++ Diabetic Diary/ViewModels/Menu.cs
@@ -0,0 +1,41 @@
+﻿using Diabetic_Diary.Models;
+
+namespace Diabetic_Diary.ViewModels
+{
+    internal class Menu
+    {
+        private RelayCommand? _openNewRecordWindowCommand;
+        public RelayCommand OpenNewRecordWindowCommand
+        {
+            get
+            {
+                return _openNewRecordWindowCommand ??= new RelayCommand(obj =>
+                {
+                    var newRecordWindow = new Views.NewRecord()
+                    {
+                        Owner = App.Current.MainWindow
+                    };
+
+                    newRecordWindow.ShowDialog();
+                });
+            }
+        }
+
+        private RelayCommand? _openHistoryWindowCommand;
+        public RelayCommand OpenHistoryWindowCommand
+        {
+            get
+            {
+                return _openHistoryWindowCommand ??= new RelayCommand(obj =>
+                {
+                    var historyWindow = new Views.History()
+                    {
+                        Owner = App.Current.MainWindow
+                    };
+
+                    historyWindow.ShowDialog();
+                });
+            }
+        }
+    }
+}
---
+++ Diabetic Diary/ViewModels/NewRecord.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Diabetic_Diary.ViewModels
+{
+    class NewRecord : INotifyPropertyChanged
+    {
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
