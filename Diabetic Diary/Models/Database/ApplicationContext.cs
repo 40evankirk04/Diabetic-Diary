f6bf5b7ec9279564bf740e0e0f1391d8ecd5b5df
Ivan <evankirk@vk.com>
2024-01-23 22:30:43 +0500

    Подключил EFC SQLite и добавил класс таблицы записей

---
+++ Diabetic Diary/Models/Database/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Diabetic_Diary.Models.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Record> Records => Set<Record>();
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=diary_history.db");
+        }
+    }
+}
---
+++ Diabetic Diary/Models/Database/Record.cs
@@ -0,0 +1,21 @@
+﻿namespace Diabetic_Diary.Models.Database
+{
+    public class Record
+    {
+        public Record() { }
+        public Record(string date, string time, double sugarValue, double eatenBreadUnits, double deliveredInsulinUnits)
+        {
+            Date = date;
+            Time = time;
+            SugarValue = sugarValue;
+            EatenBreadUnits = eatenBreadUnits;
+            DeliveredInsulinUnits = deliveredInsulinUnits;
+        }
+        public uint Id { get; set; }
+        public string? Date { get; set; }
+        public string? Time { get; set; }
+        public double SugarValue { get; set; }
+        public double EatenBreadUnits { get; set; }
+        public double DeliveredInsulinUnits { get; set; }
+    }
+}
