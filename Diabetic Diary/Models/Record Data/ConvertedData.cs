710e0030242740622656f0646462b19596693b2e
Ivan <evankirk@vk.com>
2024-01-25 02:11:10 +0500

    Класс, конвертирующий проверенные входные данные в нужные типы

---
+++ Diabetic Diary/Models/Record Data/ConvertedData.cs
@@ -0,0 +1,20 @@
+﻿namespace Diabetic_Diary.Models.Record_Data
+{
+    public class ConvertedData
+    {
+        public ConvertedData(InputedData inputedData)
+        {
+            Date = Convert.ToDateTime(inputedData.Date);
+            Time = Convert.ToDateTime(inputedData.Time);
+            SugarValue = Convert.ToDouble(inputedData.SugarValue);
+            EatenBreadUnits = Convert.ToDouble(inputedData.EatenBreadUnits);
+            DeliveredInsulinUnits = Convert.ToDouble(inputedData.DeliveredInsulinUnits);
+        }
+
+        public DateTime Date { get; private set; }
+        public DateTime Time { get; private set; }
+        public double SugarValue { get; private set; }
+        public double EatenBreadUnits { get; private set; }
+        public double DeliveredInsulinUnits { get; private set; }
+    }
+}
