23ab3817995a42c01eda56f8df05848f8a7f65bd
Ivan <evankirk@vk.com>
2024-01-25 02:11:57 +0500

    Метод установки конвертированных значений в свойства класса

--- Diabetic Diary/Models/Database/Record.cs
+++ Diabetic Diary/Models/Database/Record.cs
@@ -1,21 +1,23 @@
-﻿namespace Diabetic_Diary.Models.Database
+﻿using Diabetic_Diary.Models.Record_Data;
+
+namespace Diabetic_Diary.Models.Database
 {
     public class Record
     {
-        public Record() { }
-        public Record(string date, string time, double sugarValue, double eatenBreadUnits, double deliveredInsulinUnits)
-        {
-            Date = date;
-            Time = time;
-            SugarValue = sugarValue;
-            EatenBreadUnits = eatenBreadUnits;
-            DeliveredInsulinUnits = deliveredInsulinUnits;
-        }
         public uint Id { get; set; }
-        public string? Date { get; set; }
-        public string? Time { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
         public double SugarValue { get; set; }
         public double EatenBreadUnits { get; set; }
         public double DeliveredInsulinUnits { get; set; }
+
+        public void SetValues(ConvertedData convertedData)
+        {
+            Date = convertedData.Date;
+            Time = convertedData.Time;
+            SugarValue = convertedData.SugarValue;
+            EatenBreadUnits = convertedData.EatenBreadUnits;
+            DeliveredInsulinUnits = convertedData.DeliveredInsulinUnits;
+        }
     }
 }
