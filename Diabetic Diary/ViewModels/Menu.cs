db6d9911ac4bf843271361e7b42f43fce6aea5d5
Ivan <evankirk@vk.com>
2024-01-28 02:28:23 +0500

    Открытие окон вынесено в отдельные методы

--- Diabetic Diary/ViewModels/Menu.cs
+++ Diabetic Diary/ViewModels/Menu.cs
@@ -9,15 +9,7 @@ namespace Diabetic_Diary.ViewModels
         {
             get
             {
-                return _openNewRecordWindowCommand ??= new RelayCommand(obj =>
-                {
-                    var newRecordWindow = new Views.NewRecord()
-                    {
-                        Owner = App.Current.MainWindow
-                    };
-
-                    newRecordWindow.ShowDialog();
-                });
+                return _openNewRecordWindowCommand ??= new RelayCommand(obj => OpenNewRecordWindow());
             }
         }
 
@@ -26,16 +18,33 @@ namespace Diabetic_Diary.ViewModels
         {
             get
             {
-                return _openHistoryWindowCommand ??= new RelayCommand(obj =>
-                {
-                    var historyWindow = new Views.History()
-                    {
-                        Owner = App.Current.MainWindow
-                    };
-
-                    historyWindow.ShowDialog();
-                });
+                return _openHistoryWindowCommand ??= new RelayCommand(obj => OpenHistoryWindowAsync(), obj => true);
             }
+        }
+
+        private async void OpenHistoryWindowAsync()
+        {
+            History historyVM = new();
+
+            await Task.Run(historyVM.GetRecordsFromDBAsync);
+
+            var historyWindow = new Views.History()
+            {
+                Owner = App.Current.MainWindow,
+                DataContext = historyVM
+            };
+
+            historyWindow.ShowDialog();
+        }
+
+        private void OpenNewRecordWindow()
+        {
+            var newRecordWindow = new Views.NewRecord()
+            {
+                Owner = App.Current.MainWindow
+            };
+
+            newRecordWindow.ShowDialog();
         }
     }
 }
