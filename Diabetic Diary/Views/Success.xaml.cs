60d4899a9228c7ac009a9dec3377cfdd4c3427f9
Ivan <evankirk@vk.com>
2024-01-28 02:32:45 +0500

    Создано View успешного сохранения

---
+++ Diabetic Diary/Views/Success.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Diabetic_Diary.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для Success.xaml
+    /// </summary>
+    public partial class Success : Window
+    {
+        public Success()
+        {
+            InitializeComponent();
+        }
+    }
+}
